69dfd9df322b6073b026be3a9f8957a799a95a38
Sergio Ruiz <sergiotto@outlook.com>
2019-09-07 19:37:00 +0200

    trayendo datos del LS
    agregando funcion para obtener del local storage y pintar por pantalla

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,22 +7,62 @@ function eventListeners() {
   document
     .querySelector("#formulario")
     .addEventListener("submit", agregarTweet);
+  listaTweets.addEventListener("click", borrarTweet);
+  document.addEventListener("DOMContentLoaded", localStorageListo);
 }
 
 function agregarTweet(e) {
   e.preventDefault();
-  const tweet = document.getElementById('tweet').value;
-  
-  const botonBorrar = document.createElement('a');
-  botonBorrar.classList='borrar-tweet';
-  botonBorrar.innerText = 'x';
-  
-  
-  const li = document.createElement('li');
+  const tweet = document.getElementById("tweet").value;
+
+  const botonBorrar = document.createElement("a");
+  botonBorrar.classList = "borrar-tweet";
+  botonBorrar.innerText = "x";
+
+  const li = document.createElement("li");
   li.innerText = tweet;
   li.appendChild(botonBorrar);
   listaTweets.appendChild(li);
 
-  console.log(li);
-  
+  // Añadimos a Local Storage
+  agregarTweetLocalStorage(tweet);
+}
+
+function borrarTweet(e) {
+  e.preventDefault();
+  if (e.target.className === "borrar-tweet") {
+    e.target.parentElement.remove();
+    alert("Tweet eliminado");
+  }
+}
+
+function agregarTweetLocalStorage(tweet) {
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  tweets.push(tweet);
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+}
+
+function obtenerTweetsLocalStorage() {
+  let tweets;
+  if (localStorage.getItem("tweets") === null) {
+    tweets = [];
+  } else {
+    tweets = JSON.parse(localStorage.getItem("tweets"));
+  }
+  return tweets;
+}
+function localStorageListo() {
+  let tweets;
+  tweets = obtenerTweetsLocalStorage();
+  tweets.forEach(function(tweet) {
+    const botonBorrar = document.createElement("a");
+    botonBorrar.classList = "borrar-tweet";
+    botonBorrar.innerText = "x";
+
+    const li = document.createElement("li");
+    li.innerText = tweet;
+    li.appendChild(botonBorrar);
+    listaTweets.appendChild(li);
+  });
 }
